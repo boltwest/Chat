cd99255e39e722c780a93e83082422e29f36016d
boltwest <boltwest@gmail.com>
2018-03-10 09:42:40 +0300

    добавил шаблонизатор

--- public/js/script.js
+++ public/js/script.js
@@ -5,11 +5,10 @@ $.ajax({
 	success: successs,
 	dataType: 'json'
 });
+
 function successs(data) {
 	console.log(data);
 }
-
-
 
 
 var socket = io();
@@ -17,7 +16,6 @@ socket.on('message', function (data) {
 	console.log(data);
 	writeText(data.text);
 });
-
 
 
 key.addEventListener('click', function (event) {
@@ -34,6 +32,93 @@ key.addEventListener('click', function (event) {
 function writeText(texts) {
 	$('#room ul').append($('<li>').text(texts));
 }
+
+const FIELD = $('#chatWrapper');
+let controlChat = new ChatController();
+controlChat.start(null, FIELD);
+
+
+
+let messageObject = {
+	name: 'Boba',
+	text: 'lorem10',
+	date: '23:18'
+};
+let templateUser = document.getElementById('message-user-template');
+
+let viewChat = new ChatView();
+viewChat.start(null, FIELD);
+viewChat.addMessageUsers(messageObject, templateUser);
+
+
+
+//                          Control                        //  Control  //
+
+function ChatController() {
+	let modelComponent;
+	let domField;
+	let textMessage = ($('#inputField .inputField__inputUserMessage', domField))[0];
+
+	this.start = function (model, field) {
+		modelComponent = model;
+		domField = field;
+		let buttonSend = ($('#inputField .inputField__buttonSend', field))[0];
+		buttonSend.addEventListener('click', this.sendMessage);
+	}
+	this.sendMessage = function () {
+		console.log(textMessage.value);
+		textMessage.value = '';
+		textMessage.focus();
+	}
+}
+
+//                          Model                          //  Model  //
+
+function ChatModel() {
+	let viewComponent;
+	let registration = false;
+
+	this.start = function (view) {
+		viewComponent = view;
+	}
+}
+
+//                          View                          //  View    //
+
+function ChatView() {
+	let modelComponent;
+	let domField;
+	let messageField;
+	let messageObject = {
+		name: 'Boba',
+		text: 'lorem10',
+		date: '23:18'
+	};
+
+	this.start = function (model, field) {
+		modelComponent = model;
+		domField = field;
+		messageField = ($('#messageField', field))[0];
+		// console.log(messageField);
+	};
+
+	this.addMessageUsers = function (json, userTemplate) {
+		let template = Handlebars.compile($(userTemplate).html());
+		$(messageField).append(template(json));
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
