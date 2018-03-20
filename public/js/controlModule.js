1ed1d51ddbf115ccc8bdfeafe93eff96c804ebbe
boltwest <boltwest@gmail.com>
2018-03-20 19:09:13 +0300

    added send message from press key enter

--- public/js/controlModule.js
+++ public/js/controlModule.js
@@ -4,15 +4,32 @@ chat.control = (function () {
 	let domField;
 	let textMessage;
 
+	function checkKeyPressMessage(event) {
+		if(event.keyCode === 13){
+			if(!event.shiftKey) {
+				my.sendMessage();
+				event.preventDefault();
+			}
+		}
+	}
+
+	function checkKeyPress(event) {
+		if(event.keyCode === 13){
+			my.checkIn();
+		}
+	}
+
 	my.init = function (model, field) {
 		modelComponent = model;
 		domField = field;
 		textMessage = ($('#inputField .inputField__inputUserMessage', field))[0];
-
+		textMessage.addEventListener('keydown', checkKeyPressMessage);
 		let buttonSend = ($('#inputField .inputField__buttonSend', field))[0];
 		buttonSend.addEventListener('click', my.sendMessage);
 		let buttonLogIn = ($('#checkIn .checkIn__button', field))[0];
 		buttonLogIn.addEventListener('click', my.checkIn);
+		let inputPasswordField = ($('#checkIn .checkIn__password', field))[0];
+		inputPasswordField.addEventListener('keydown', checkKeyPress);
 		let SowListOnline = ($('#listUsers', field))[0];
 		SowListOnline.addEventListener('click', my.showListOnline);
 	};
