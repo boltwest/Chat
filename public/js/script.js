0752724f6192e43eb5b34a0b785ae7eabb452204
boltwest <boltwest@gmail.com>
2018-03-25 14:40:58 +0300

    fix PWA to self Service Worker

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,14 @@
-const FIELD = ($('#chatWrapper'))[0];
+if ('serviceWorker' in navigator) {
+	navigator.serviceWorker.register('sw.js').then(function(reg) {
+		// регистрация сработала
+		console.log('Registration succeeded. Scope is ' + reg.scope);
+	}).catch(function(error) {
+		// регистрация прошла неудачно
+		console.log('Registration failed with ' + error);
+	});
+};
 
+const FIELD = ($('#chatWrapper'))[0];
 chat.model.init(chat.view);
 chat.view.init(chat.model, FIELD);
 chat.control.init(chat.model, FIELD);
--- public/sw.js
+++ public/sw.js
@@ -1,14 +1,31 @@
-'use strict';
-importScripts('sw-toolbox.js');
-toolbox.precache([
-	"/",
-	"index.html",
-	"css/style.css",
-	"css/media.css",
-	"js/modelModule.js",
-	"js/viewModule.js",
-	"js/controlModule.js",
-	"js/script.js",
-	"js/handlebars-v4.0.11.js"]);
-toolbox.router.get('/images/*', toolbox.cacheFirst);
-toolbox.router.get('/*', toolbox.networkFirst, {networkTimeoutSeconds: 5});
+self.addEventListener('install', event => {
+	event.waitUntil(
+		caches.open('v1')
+			.then(cache => {
+				return cache.addAll([
+					'/',
+					'index.html',
+					'images/titleicon50.png',
+					'fontactic.css',
+					'css/style.css',
+					'css/media.css',
+					'js/handlebars-v4.0.11.js',
+					'js/modelModule.js',
+					'js/viewModule.js',
+					'js/controlModule.js',
+					'js/script.js'
+				]);
+			})
+	);
+});
+
+self.addEventListener('fetch', function (event) {
+	event.respondWith(
+		caches.match(event.request)
+			.then( resp => {
+				return resp || fetch(event.request)
+					.then(response => response)
+					.catch(err => console.log('Offline' + err))
+			})
+	);
+});
